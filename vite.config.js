7482956a24f73cd021ce14221becfc86cd83c874
Soyeon0123 <switle247@naver.com>
2026-02-20 11:14:44 -0500

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   
-  base: '/Garden-of-Liberation',
+  base: '/Garden-of-Liberation/',
   plugins: [react()],
   
   // Three.js 최적화
